60f4112730941d30397224d376c4b6e357232dcc
s.touqeer@pharmatrace.io <s.touqeer@pharmatrace.io>
2024-09-15 22:05:33 +0200

    add router filter , save last page state , pass state with URL , change Routes structer for loader

--- routes-example/src/App.jsx
+++ routes-example/src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import Layout from "./components/Layout"
 import Home from "./pages/Home"
 import About from "./pages/About"
@@ -11,12 +16,40 @@ import HostLayout from "./components/HostLayout"
 import HostVans from "./pages/Host//HostVans"
 import HostVanInfo from "./pages/Host/HostVanInfo"
 import HostVanPricing from "./pages/Host/HostVanPrice"
-import HostVanPhotos from "./pages/Host//HostVanPhotos"
-import HostVanDetails from "./pages/Host//HostVanDetails"
+import HostVanPhotos from "./pages/Host/HostVanPhotos"
+import HostVanDetails from "./pages/Host/HostVanDetails"
+import NotFound from "./pages/NotFound"
+
 
 
 // import HostVanLayout from "./components/HostVanLayout"
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+      <Route element={<Layout />}>
+
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="vans" element={<Vans />} />
+        <Route path="vans/:id" element={<VanDetail />} />
+        <Route path="host" element={<HostLayout />} >
+          <Route index element={<Dashboard />} />
+          <Route path="income" element={<Income />} />
+          <Route path="reviews" element={<Reviews />} />
+          <Route path="vans" element={<HostVans />} />
+          <Route path="vans/:id" element={<HostVanDetails />} >
+            <Route index element={<HostVanInfo />} />
+            <Route path="pricing" element={<HostVanPricing />} />
+            <Route path="photos" element={<HostVanPhotos />} />
+          </Route>
+
+        </Route>
+        <Route path="*" element={<NotFound />} />
+
+      </Route>
+
+  )
+);
 
 
 import './App.css'
@@ -27,32 +60,8 @@ import "./server"
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <RouterProvider router={router} />
 
-        <Routes>
-          <Route element={<Layout />}>
-
-            <Route path="/" element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="vans" element={<Vans />} />
-            <Route path="vans/:id" element={<VanDetail />} />
-            <Route path="host" element={<HostLayout />} >
-              <Route index element={<Dashboard />} />
-              <Route path="income" element={<Income />} />
-              <Route path="reviews" element={<Reviews />} />
-              <Route path="vans" element={<HostVans />} />
-              <Route path="vans/:id" element={<HostVanDetails />} >
-                <Route index element={<HostVanInfo />} />
-                <Route path="pricing" element={<HostVanPricing />} />
-                <Route path="photos" element={<HostVanPhotos />} />
-              </Route>
-
-            </Route>
-
-          </Route>
-
-        </Routes>
-      </BrowserRouter>
     </>
   )
 }
